c1cbb287f92a2f0095a710979835270a62e68c8c
deroffal <32491733+deroffal@users.noreply.github.com>
2025-12-02 14:59:12 +0100

    day02 - part2

--- src/main/kotlin/fr/deroffal/aoc2025/Day02.kt
+++ src/main/kotlin/fr/deroffal/aoc2025/Day02.kt
@@ -6,10 +6,11 @@ class IdRange(val range: LongRange) {
 
     constructor(input: String) : this(input.split("-").let { LongRange(it[0].toLong(), it[1].toLong()) })
 
-    fun countRepeatTwice()= range.filter { repeatTwice(it) }.sum()
+    fun countRepeatingTwice()= range.filter { isRepeatingTwice(it) }.sum()
+    fun countRepeating()= range.filter { isRepeating(it) }.sum()
 }
 
-fun repeatTwice(id: Long): Boolean {
+fun isRepeatingTwice(id: Long): Boolean {
     val toString = id.toString()
     if (toString.length % 2 != 0) return false
 
@@ -18,16 +19,29 @@ fun repeatTwice(id: Long): Boolean {
     return toString.take(half) == toString.substring(half)
 }
 
+fun isRepeating(id: Long): Boolean {
+        val toString = id.toString()
+        val length = toString.length
+
+        return  (1..length / 2).any { toString.chunked(it).toSet().size == 1 }
+}
+
 val day02Input = File(ClassLoader.getSystemResource("day02.txt").toURI()).readText().trim()
 
 class Day02(val input: String) {
     fun part1() = input.split(",")
         .map { range -> IdRange(range) }
-        .sumOf { it.countRepeatTwice() }
+        .sumOf { it.countRepeatingTwice() }
+
+    fun part2() = input.split(",")
+        .map { range -> IdRange(range) }
+        .sumOf { it.countRepeating() }
 }
 
 fun main(){
+    val day02 = Day02(day02Input)
     println("""
-        part 1 : ${Day02(day02Input).part1()}
+        part 1 : ${day02.part1()}
+        part 2 : ${day02.part2()}
     """.trimIndent())
 }
--- src/test/kotlin/fr/deroffal/aoc2025/Day02Test.kt
+++ src/test/kotlin/fr/deroffal/aoc2025/Day02Test.kt
@@ -12,28 +12,28 @@ class Day02Test {
 
     @Test
     fun `is invalid`() {
-        assertTrue(repeatTwice(11))
-        assertTrue(repeatTwice(22))
-        assertTrue(repeatTwice(99))
-        assertTrue(repeatTwice(1010))
-        assertTrue(repeatTwice(1188511885))
-        assertTrue(repeatTwice(222222))
-        assertTrue(repeatTwice(446446))
-        assertTrue(repeatTwice(38593859))
+        assertTrue(isRepeatingTwice(11))
+        assertTrue(isRepeatingTwice(22))
+        assertTrue(isRepeatingTwice(99))
+        assertTrue(isRepeatingTwice(1010))
+        assertTrue(isRepeatingTwice(1188511885))
+        assertTrue(isRepeatingTwice(222222))
+        assertTrue(isRepeatingTwice(446446))
+        assertTrue(isRepeatingTwice(38593859))
     }
 
     @Test
     fun `is valid`() {
-        assertFalse(repeatTwice(111))
-        assertFalse(repeatTwice(1001))
+        assertFalse(isRepeatingTwice(111))
+        assertFalse(isRepeatingTwice(1001))
     }
 
     @Test
     fun `count invalid in range`(){
-        assertEquals(2, IdRange("11-22").range.count { repeatTwice(it) })
-        assertEquals(1, IdRange("95-115").range.count { repeatTwice(it) })
-        assertEquals(1, IdRange("998-1012").range.count { repeatTwice(it) })
-        assertEquals(1, IdRange("1188511880-1188511890").range.count { repeatTwice(it) })
+        assertEquals(2, IdRange("11-22").range.count { isRepeatingTwice(it) })
+        assertEquals(1, IdRange("95-115").range.count { isRepeatingTwice(it) })
+        assertEquals(1, IdRange("998-1012").range.count { isRepeatingTwice(it) })
+        assertEquals(1, IdRange("1188511880-1188511890").range.count { isRepeatingTwice(it) })
     }
 
     @Test
@@ -49,5 +49,32 @@ class Day02Test {
         assertEquals(37314786486, day02.part1())
     }
 
+    @Test
+    fun isRepeating(){
+        //part 1 example
+        assertTrue(isRepeating(11))
+        assertTrue(isRepeating(22))
+        assertTrue(isRepeating(99))
+        assertTrue(isRepeating(1010))
+        assertTrue(isRepeating(1188511885))
+        assertTrue(isRepeating(222222))
+        assertTrue(isRepeating(446446))
+        assertTrue(isRepeating(38593859))
 
+        assertTrue(isRepeating(111))
+        assertFalse(isRepeating(1001))
+
+        //
+        assertTrue(isRepeating(1010))
+        assertTrue(isRepeating(999))
+        assertTrue(isRepeating(565656))
+        assertTrue(isRepeating(824824824))
+        assertTrue(isRepeating(2121212121))
+    }
+
+    @Test
+    fun `part2 - verify example`(){
+        val day02 = Day02(testInput)
+        assertEquals(4174379265, day02.part2())
+    }
 }
