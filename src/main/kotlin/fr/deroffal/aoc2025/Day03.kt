8f08ef5f7439fe0fa14f7f06bf0ea2b5a9d5c2b0
deroffal <32491733+deroffal@users.noreply.github.com>
2025-12-03 08:16:20 +0100

    day03 - part2

--- src/main/kotlin/fr/deroffal/aoc2025/Day03.kt
+++ src/main/kotlin/fr/deroffal/aoc2025/Day03.kt
@@ -1,10 +1,15 @@
 package fr.deroffal.aoc2025
 
 import java.io.File
+import kotlin.math.pow
 
-class Banks(val banks: List<Bank>) {
-    constructor(input: String) : this(input.toCharArray().mapIndexed { index, char -> Bank(char.digitToInt(), index) })
+class Battery(val banks: List<Bank>) {
+    constructor(input: String) : this(
+        input.toCharArray()
+            .mapIndexed { index, char -> Bank(char.digitToInt(), index) }
+    )
 
+    //keeping for part 1, but can be replaced with getMaxVoltage(2)
     fun getMaxVoltage(): Int {
         var mostPowerful = banks.maxBy { it.voltage }
 
@@ -16,21 +21,44 @@ class Banks(val banks: List<Bank>) {
                 banks.filter { it.position > mostPowerful.position }
                     .maxOf { it.voltage }
     }
+
+    fun getMaxVoltage(size: Int): Long {
+        var battery = this
+        var voltage = 0L
+
+        for (i in size - 1 downTo 0) {
+            val mostPowerful = battery.banks
+                .take(battery.banks.size - i) // candidates must leave enough banks for the remaining positions
+                .maxBy { it.voltage }
+            voltage += mostPowerful.voltage * 10.0.pow(i.toDouble()).toLong()
+            battery = battery.dropBefore(mostPowerful.position) // drop used banks and those before
+        }
+
+        return voltage
+    }
+
+    fun dropBefore(position: Int) = banks.filterNot { it.position <= position }
+        .mapIndexed { index, bank -> Bank(bank.voltage, index) }
+        .run { Battery(this) }
+
+    override fun toString() = banks.map { it.voltage }.joinToString("")
 }
 
-class Bank(val voltage: Int, val position: Int) {
+data class Bank(val voltage: Int, val position: Int)
 
-}
-
-val day03Input = File(ClassLoader.getSystemResource("day03.txt").toURI()).readLines().filterNot { it.isBlank()  }
+val day03Input = File(ClassLoader.getSystemResource("day03.txt").toURI()).readLines().filterNot { it.isBlank() }
 
 class Day03(val inputs: List<String>) {
-    fun part1() = inputs.sumOf { Banks(it).getMaxVoltage() }
+    fun part1() = inputs.sumOf { Battery(it).getMaxVoltage() }
+    fun part2() = inputs.sumOf { Battery(it).getMaxVoltage(12) }
 }
 
 fun main() {
     val day03 = Day03(day03Input)
-    println("""
+    println(
+        """
         part 1 : ${day03.part1()}
-    """.trimIndent())
+        part 2 : ${day03.part2()}
+    """.trimIndent()
+    )
 }
--- src/test/kotlin/fr/deroffal/aoc2025/Day03Test.kt
+++ src/test/kotlin/fr/deroffal/aoc2025/Day03Test.kt
@@ -1,6 +1,8 @@
 package fr.deroffal.aoc2025
 
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Nested
+import java.io.File
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -15,15 +17,15 @@ class Day03Test {
 
     @Test
     fun `create bank from input`() {
-        val banks = Banks("987654321111111").banks
+        val battery = Battery("987654321111111").banks
 
-        assertEquals(15, banks.size)
+        assertEquals(15, battery.size)
 
-        val banks1 = banks[0]
+        val banks1 = battery[0]
         assertEquals(9, banks1.voltage)
         assertEquals(0, banks1.position)
 
-        val banks2 = banks[1]
+        val banks2 = battery[1]
         assertEquals(8, banks2.voltage)
         assertEquals(1, banks2.position)
 
@@ -34,21 +36,71 @@ class Day03Test {
 
         @Test
         fun `when banks are sorted`() {
-            assertEquals(98, Banks("987654321111111").getMaxVoltage())
-            assertEquals(92, Banks("818181911112111").getMaxVoltage())
+            assertEquals(98, Battery("987654321111111").getMaxVoltage())
+            assertEquals(98, Battery("987654321111111").getMaxVoltage(2))
+            assertEquals(92, Battery("818181911112111").getMaxVoltage())
+            assertEquals(92, Battery("818181911112111").getMaxVoltage(2))
         }
 
         @Test
         fun `when most powerful is last`() {
-            assertEquals(89, Banks("811111111111119").getMaxVoltage())
-            assertEquals(78, Banks("234234234234278").getMaxVoltage())
+            assertEquals(89, Battery("811111111111119").getMaxVoltage())
+            assertEquals(89, Battery("811111111111119").getMaxVoltage(2))
+            assertEquals(78, Battery("234234234234278").getMaxVoltage())
+            assertEquals(78, Battery("234234234234278").getMaxVoltage(2))
         }
 
     }
 
     @Test
-    fun `part 1`() {
+    fun `part 1 - example`() {
         val day03 = Day03(inputs)
         assertEquals(357, day03.part1())
+    }
+
+    val day03Input = File(ClassLoader.getSystemResource("day03.txt").toURI()).readLines().filterNot { it.isBlank() }
+
+    @Test
+    fun `part 1 - input`() {
+        val day03 = Day03(day03Input)
+        assertEquals(17403, day03.part1())
+    }
+
+    @Nested
+    inner class GetMaxVoltageWithSize{
+
+        @Test
+        fun `when banks are sorted - size 2`() {
+            assertEquals(98L, Battery("987654321111111").getMaxVoltage(2))
+        }
+
+        @Test
+        fun `when banks are sorted - size 3`() {
+            assertEquals(987L, Battery("987654321111111").getMaxVoltage(3))
+        }
+
+        @Test
+        fun `when banks are sorted - size 12`() {
+            assertEquals(987654321111L, Battery("987654321111111").getMaxVoltage(12))
+        }
+
+        @Test
+        fun `when banks are not sorted - size 12`() {
+            assertEquals(811111111119L, Battery("811111111111119").getMaxVoltage(12))
+            assertEquals(434234234278L, Battery("234234234234278").getMaxVoltage(12))
+            assertEquals(888911112111L, Battery("818181911112111").getMaxVoltage(12))
+        }
+    }
+
+    @Test
+    fun `part 2 - example`() {
+        val day03 = Day03(inputs)
+        assertEquals(3121910778619L, day03.part2())
+    }
+
+    @Test
+    fun `part 2`() {
+        val day03 = Day03(day03Input)
+        assertEquals(173416889848394L, day03.part2())
     }
 }
